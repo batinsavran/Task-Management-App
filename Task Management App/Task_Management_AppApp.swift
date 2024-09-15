370cda15f7e88c77c751499e3f9c661129612194
Oğulcan Aslan <batinsavran00@gmail.com>
2024-09-16 00:09:14 +0300

    Initial Commit

---
+++ Task Management App/Task_Management_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_Management_AppApp.swift
+//  Task Management App
+//
+//  Created by mobiroller on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Task_Management_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
