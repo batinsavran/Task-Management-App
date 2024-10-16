4094a98de6a040b61876060d02b55ab31fcfaf5c
Oğulcan Aslan <batinsavran00@gmail.com>
2024-10-17 00:01:09 +0300

    Add isSameHour and isPast computed properties to Date extension
    - Added `isSameHour` computed property to check if the current date falls within the same hour as the provided date.
    - Added `isPast` computed property to determine if the current date is earlier than the provided date when compared at the hour level.
    - These methods will help simplify date comparisons in the app where granularity down to the hour is needed.

--- Task Management App/Helpers/Date+Extensions.swift
+++ Task Management App/Helpers/Date+Extensions.swift
@@ -12,6 +12,14 @@ extension Date {
         return Calendar.current.isDateInToday(self)
     }
     
+    var isSameHour: Bool {
+        return Calendar.current.compare(self, to: .init(), toGranularity: .hour) == .orderedSame
+    }
+    
+    var isPast: Bool {
+        return Calendar.current.compare(self, to: .init(), toGranularity: .hour) == .orderedAscending
+    }
+    
     func fetchWeek(_ date: Date = .init()) -> [WeekDay] {
         let calendar = Calendar.current
         let startOfDate = calendar.startOfDay(for: date)
