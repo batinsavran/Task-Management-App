faa53190075e39ebb0c3c5c4ec4a11e378f15fa8
Oğulcan Aslan <batinsavran00@gmail.com>
2024-10-19 14:41:29 +0300

    Refactor NewTaskView to save tasks using SwiftData
    Updated NewTaskView to include task creation functionality. Integrated model context to insert new tasks with title, date, and color. Adjusted task color representation to use strings instead of Color type.

--- Task Management App/View/NewTaskView.swift
+++ Task Management App/View/NewTaskView.swift
@@ -2,9 +2,10 @@ import SwiftUI
 
 struct NewTaskView: View {
     @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
     @State private var taskTitle: String = ""
     @State private var taskDate: Date = .init()
-    @State private var taskColor: Color = .taskColor1
+    @State private var taskColor: String = "TaskColor 1"
     
     var body: some View {
         VStack(alignment: .leading, spacing: 15, content: {
@@ -47,12 +48,14 @@ struct NewTaskView: View {
                         .font(.caption)
                         .foregroundStyle(.gray)
                     
-                    let colors: [Color] = [.taskColor1, .taskColor2, .taskColor3, .taskColor4, .taskColor5]
+                    let colors: [String] = (1...5).compactMap { index -> String in
+                        return "TaskColor \(index)"
+                    }
                     
                     HStack(spacing: 0) {
                         ForEach(colors, id: \.self) { color in
                             Circle()
-                                .fill(color)
+                                .fill(Color(color))
                                 .frame(width: 20, height: 20)
                                 .background(content: {
                                     Circle()
@@ -74,7 +77,16 @@ struct NewTaskView: View {
             
             Spacer(minLength: 0)
             
-            Button(action: {}, label: {
+            Button(action: {
+                let task = Task(taskTitle: taskTitle, creationDate: taskDate, tint: taskColor)
+                do {
+                    context.insert(task)
+                    try context.save()
+                    dismiss()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }, label: {
                 Text("Create Task")
                     .font(.title3)
                     .fontWeight(.semibold)
@@ -82,7 +94,7 @@ struct NewTaskView: View {
                     .foregroundStyle(.black)
                     .hSpacing(.center)
                     .padding(.vertical, 12)
-                    .background(taskColor, in: .rect(cornerRadius: 10))
+                    .background(Color(taskColor), in: .rect(cornerRadius: 10))
             })
             .disabled(taskTitle == "")
             .opacity(taskTitle == "" ? 0.5 : 1)
